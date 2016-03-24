0d0181782ac744876709a43d89780bb923505634
ryankroll <rmkroll@hotmail.com>
2016-03-24 12:56:17 -0400

    Update company information and pull company information from parse.

--- app/src/main/java/com/example/rkroll/auto_scheduler/CompanyInformation.java
+++ app/src/main/java/com/example/rkroll/auto_scheduler/CompanyInformation.java
@@ -5,8 +5,17 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
+
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 
 public class CompanyInformation extends AppCompatActivity {
+
+    private TextView companyName;
+    private TextView storeId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,19 +24,30 @@ public class CompanyInformation extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        //FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        //fab.setOnClickListener(new View.OnClickListener() {
-        //    @Override
-        //    public void onClick(View view) {
-        //        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-        //                .setAction("Action", null).show();
-        //    }
-        //});
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        companyName = (TextView) findViewById(R.id.companyName);
+        storeId = (TextView) findViewById(R.id.storeId);
+
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Store");
+        query.whereEqualTo("companyName", "Wendy's");
+        query.getFirstInBackground(new GetCallback<ParseObject>() {
+            @Override
+            public void done(ParseObject object, ParseException e) {
+                if (e == null) {
+                    objectRetrivalSuccessful(object);
+                } else {
+                    objectRetrivalFailed();
+                }
+            }
+
+        });
     }
 
     public void editInfo(View view){
         Intent intent = new Intent(this, EditCompanyInformation.class);
+        intent.putExtra("EXTRA_COMPANY_NAME", companyName.getText().toString());
+        intent.putExtra("EXTRA_STOREID", storeId.getText().toString());
         startActivity(intent);
     }
 
@@ -35,5 +55,14 @@ public class CompanyInformation extends AppCompatActivity {
         Intent intent = new Intent(this, EditStoreHours.class);
         startActivity(intent);
     }
+
+    public void objectRetrivalSuccessful(ParseObject object) {
+        companyName.setText(object.getString("companyName"));
+        storeId.setText(String.valueOf(object.getInt("storeID")));
+    };
+    public void objectRetrivalFailed() {
+
+    };
+
 
 }
--- app/src/main/java/com/example/rkroll/auto_scheduler/EditCompanyInformation.java
+++ app/src/main/java/com/example/rkroll/auto_scheduler/EditCompanyInformation.java
@@ -1,10 +1,31 @@
 package com.example.rkroll.auto_scheduler;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.parse.FindCallback;
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.SaveCallback;
+
 
 public class EditCompanyInformation extends AppCompatActivity {
+
+    private EditText companyEditText;
+    private EditText storeIdEditText;
+    private Button saveButton;
+    private TextView companyNameTextView;
+    private TextView storeIdTextView;
+    ParseObject currentCompany = new ParseObject("Store");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,15 +34,69 @@ public class EditCompanyInformation extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        companyEditText = ((EditText) findViewById(R.id.editCompanyName));
+        storeIdEditText = ((EditText) findViewById(R.id.editStoreID));
+
+        saveButton = (Button) findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(saveButtonListener);
+
+        companyNameTextView= (TextView) findViewById(R.id.companyName);
+        storeIdTextView = (TextView) findViewById(R.id.storeId);
+        Intent intent = getIntent();
+        String companyName = intent.getStringExtra("EXTRA_COMPANY_NAME");
+        String storeId = intent.getStringExtra("EXTRA_STOREID");
+        companyNameTextView.setText(companyName);
+        storeIdTextView.setText(storeId);
+
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Store");
+        query.whereEqualTo("companyName", "Wendy's");
+        query.getFirstInBackground(new GetCallback<ParseObject>() {
+            @Override
+            public void done(ParseObject object, ParseException e) {
+                if (e == null) {
+                    objectRetrievalSuccessful(object);
+                } else {
+                    objectRetrivalFailed();
+                }
+            }
+
+        });
+
+
     }
+
+    private final View.OnClickListener saveButtonListener = new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+
+            String newCompanyName = companyEditText.getText().toString();
+            int newStoreId = Integer.parseInt(storeIdEditText.getText().toString());
+
+            currentCompany.put("companyName", newCompanyName);
+            currentCompany.put("storeID", newStoreId);
+            currentCompany.saveInBackground(new SaveCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if (e == null) {
+                        Log.d("Success", "Success");
+                    }
+                    else {
+                        Log.d("Exception: ", e.getMessage());
+                    }
+                }
+            });
+        }
+
+    };
+
+    public void objectRetrievalSuccessful(ParseObject object) {
+        currentCompany = object;
+    };
+
+    public void objectRetrivalFailed() {
+
+    };
 
 }
