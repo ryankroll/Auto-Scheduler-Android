7694b7e2daf4ec906b470dafee37eebbbfa25776
Brian <bcheshir@umflint.edu>
2016-04-10 17:01:13 -0400

    Finished home screen view and manager view with a recycler view for the requests from Employees

--- app/src/main/java/com/example/rkroll/auto_scheduler/HomeScreen.java
+++ app/src/main/java/com/example/rkroll/auto_scheduler/HomeScreen.java
@@ -1,6 +1,7 @@
 package com.example.rkroll.auto_scheduler;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,15 +11,46 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import android.app.Activity;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TableRow.LayoutParams;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 public class HomeScreen extends AppCompatActivity {
+    TableLayout tableLayout;
+    int daysOfWeek = 7;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+        tableLayout = (TableLayout) findViewById(R.id.weekSchedule);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+
+
+
+        buildTable(7,daysOfWeek);
+
         setSupportActionBar(toolbar);
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
@@ -39,8 +71,8 @@ public class HomeScreen extends AppCompatActivity {
         if (id == R.id.action_settings) {
 
             /**************************
-            NEED TO DO CHECK HERE TO MAKE SURE USER IS GENERAL MANAGER
-            ***************************/
+             NEED TO DO CHECK HERE TO MAKE SURE USER IS GENERAL MANAGER
+             ***************************/
 
             Intent intent = new Intent(this, GeneralManager.class);
             startActivity(intent);
@@ -48,7 +80,7 @@ public class HomeScreen extends AppCompatActivity {
         }
 
         // if manager settings button is pressed from menu attempt to access manager activities
-        if (id == R.id.man_settings){
+        if (id == R.id.man_settings) {
 
             /**************************
              NEED TO DO CHECK HERE TO MAKE SURE USER IS A MANAGER
@@ -62,4 +94,91 @@ public class HomeScreen extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "HomeScreen Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.rkroll.auto_scheduler/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "HomeScreen Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.rkroll.auto_scheduler/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+
+    private void buildTable(int col, int rows) {
+        TextView tv;
+        TableRow tr;
+        String day = null;
+        String tableBox = null;
+        int dimen = (int) this.getResources().getDimension(R.dimen.tableSize);
+
+        for (int i = 1; i <= rows; i++) {
+            tr = new TableRow(this);
+            tr.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,
+                    LayoutParams.WRAP_CONTENT));
+
+            for (int j = 1; j <= col; j++) {
+                tv = new TextView(this);
+                tv.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,
+                        LayoutParams.WRAP_CONTENT));
+                tv.setPadding(5, 5, 30, 5);
+
+                if (j == 1){
+                    switch(i){
+                        case 1: day = "Sun: ";
+                            break;
+                        case 2: day = "Mon: ";
+                            break;
+                        case 3: day = "Tues: ";
+                            break;
+                        case 4: day = "Wed: ";
+                            break;
+                        case 5: day = "Thurs: ";
+                            break;
+                        case 6: day = "Fri: ";
+                            break;
+                        case 7: day = "Sat: ";
+                            break;
+                    }
+                    tv.setText(day);
+                }else{
+                    tableBox = "R" + i + ", C" + j;
+                    tv.setText(tableBox);
+                }
+                tr.addView(tv);
+
+            }
+
+            tableLayout.addView(tr);
+        }
+    }
 }
--- app/src/main/java/com/example/rkroll/auto_scheduler/Manager.java
+++ app/src/main/java/com/example/rkroll/auto_scheduler/Manager.java
@@ -4,10 +4,26 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
+
+import com.parse.ParseUser;
+
+import java.util.List;
 
 public class Manager extends AppCompatActivity {
+
+    private EmployeeAdapter adapter;
+    private RecyclerView requestRV;
+    private RecyclerView availRV;
+    LinearLayoutManager llmRequest = new LinearLayoutManager(this);
+    LinearLayoutManager llmAvail = new LinearLayoutManager(this);
+    private UserReaderDbHelper userDb;
+    private List<ParseUser> currentEmployees;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +32,42 @@ public class Manager extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // Getting the two different references for the Recycler Views
+        requestRV = (RecyclerView) findViewById(R.id.requestRecyclerView);
+        availRV = (RecyclerView) findViewById(R.id.availRecyclerView);
+
+
+        requestRV.setLayoutManager(llmRequest);
+//        adapter = new EmployeeAdapter(b, itemClickListener, itemLongClickListener);
+        requestRV.setAdapter(adapter);
+
+
+        availRV.setLayoutManager(llmAvail);
+//        adapter = new EmployeeAdapter(b, itemClickListener, itemLongClickListener);
+        availRV.setAdapter(adapter);
+
+
 
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    private final View.OnClickListener itemClickListener =
+            new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                }
+            };
+
+    private final View.OnLongClickListener itemLongClickListener =
+            new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View view) {
+                    // get the tag that the user long touched
+                    final String tag = ((TextView) view).getText().toString();
+
+                    return true;
+                }
+            };
 }
